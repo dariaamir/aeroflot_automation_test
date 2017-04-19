6398d4599fd4e5816f4cde95432c3fb71c9b92fb
dariaamir <da@bekitzur.com>
2017-04-19 19:14:48 +0300

    Changed yanl to json

--- tests/test_buy_tickets.py
+++ tests/test_buy_tickets.py
@@ -1,23 +1,26 @@
 import unittest
 from pages.start_page import StartPage
 from selenium import webdriver
+import json
 
 
 class TestBuyTickets(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome()
-        self.driver.implicitly_wait(30)
-        self.driver.maximize_window()
-        page = StartPage(self.driver)
-        self.driver.get(page.page_address)
+        with open('../config/test_data.json') as config_json:
+            self.config = json.load(config_json)
 
     def test_01_select_destination_and_dates(self):
         #!!! empty field before entering new
 
         #enter destination
         page = StartPage(self.driver)
-        page.destimation_to.click()
+        self.driver.get(page.page_address)
+        self.driver.implicitly_wait(30)
+        page_address_field = self.driver.find_element_by_class_name(page.destination_to)
+        page_address_field.clear()
+        page_address_field.send_keys(self.config['StartPage']['flight_from'])
 
         #enter passagers
         #enter dates
